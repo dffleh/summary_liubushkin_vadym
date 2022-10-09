3caa98335b7a14d9c8714d4ab2dc036450ccf2e0
Alexander Shabanov <dffleh1995@gmail.com>
2022-10-09 18:21:52 +0300

    add user

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,12 @@
+import { Profile } from "./Profile";
+import user from "./user.json"
+
 export const App = () => {
+  console.log(user);
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <Profile profile={user}/>
+      
     </div>
   );
 };
