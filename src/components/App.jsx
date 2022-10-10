580793623da5aa155c89e20c5a85c8893645b1ee
Alexander Shabanov <dffleh1995@gmail.com>
2022-10-10 12:08:48 +0300

    для моей бубочки

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,14 @@
-import { Profile } from "./Profile";
-import user from "./user.json"
+import { Profile } from './Profile';
+import { Statistics } from './Statistics';
+import user from './user.json';
+import data from './data.json';
 
 export const App = () => {
   console.log(user);
   return (
     <div>
-      <Profile profile={user}/>
-      
+      <Profile profile={user} />
+      <Statistics statistics={data} />
     </div>
   );
 };
