a982d8b8a77b49530f2db88f48942ed6094115cd
dffleh <dffleh1995@gmail.com>
2022-12-03 01:57:13 +0200

    del

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,19 +1,3 @@
-import { Profile } from './Profile/Profile';
-import { Statistics } from './Statistics/Statistics';
-import { FriendList } from './Friend/FriendList';
-import { TransactionHistory } from './TransactionHistory/TransactionHistory';
-import user from '../data/user.json';
-import data from '../data/data.json';
-import friends from '../data/friends.json';
-import transactions from '../data/transactions.json';
-
 export const App = () => {
-  return (
-    <div>
-      <Profile profile={user} />
-      <Statistics title="Upload stats" stats={data} />
-      <FriendList friends={friends} />
-      <TransactionHistory items={transactions} />
-    </div>
-  );
+  return <div>sumurry</div>;
 };
